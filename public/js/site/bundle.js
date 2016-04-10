95cef92346c8f012339ef926ee49022ebf6b2347
Evgeniy Troynov <troinov174@gmail.com>
2016-04-10 22:31:00 +0500

    move app.js in views, add text.js plugin for RequireJS, add text.js in config

--- public/js/site/bundle.js
+++ public/js/site/bundle.js
@@ -4,17 +4,22 @@ requirejs.config({
     lodash:     '../libs/lodash.min',
     jquery:     '../libs/jquery.min',
     backbone:   '../libs/backbone.min',
-    hbs: '../libs/handlebars.min'
+    hbs:        '../libs/handlebars.min',
+    text:       '../libs/text.min'
   },
   shim: {
     lodash: {
       exports: '_'
+    },
+    backbone: {
+      deps: ['jquery', 'lodash'],
+      exports: 'Backbone'
     }
   }
 });
 
-require(['jquery', 'hbs', 'app'], function($, app) {
-  console.info(window);
+require(['jquery', 'hbs', 'views/app'], function($, hbs, app) {
+  console.info(hbs);
 
   $.get('/search/ящ').done(function(data) {
       console.info('test', data);
